0842b61a83cefff032306189b03cc737291fcd5c
dustingottlieb <dustingottlieb@gmail.com>
2021-07-22 21:26:23 -0500

    fixing server

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,59 +1,71 @@
-const { User } = require('../models');
-// import authentication utility and error
 const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        //get current users information
         me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
-                .select('-__v -password')
-                return userData;
+                return User.findOne({ _id: context.user._id }).populate('savedBooks');
             }
-            throw new AuthenticationError('Not logged in');
-        }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        users: async () => {
+      return User.find().populate('savedBooks');
     },
+
+    },
+
     Mutation: {
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne( { email });
+        //signin function creates token and returns it with user data
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        //logs user in, creates and returns token with user data
+        login: async (parent, { username, email, password }) => {
+            const user = await User.findOne({ $or: [{ username: username }, { email: email }] });
             if (!user) {
-                throw new AuthenticationError('Incorrect credentials')
+                throw new AuthenticationError("Can't find this user");
             }
+
             const correctPw = await user.isCorrectPassword(password);
-            if(!correctPw) {
-                throw new AuthenticationError('Incorrect credentials')
+            if (!correctPw) {
+                throw new AuthenticationError('Wrong password!' );
             }
             const token = signToken(user);
-            return { token, user };
+
+            return {token, user}
         },
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user)
-            return { token, user };
-        },
-        saveBook: async (parent, { book }, context) => {
+        //args contains book data, is added to savedBooks in users data
+        saveBook: async (parent, args, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
-                    { $addToSet: {savedBooks: book} },
-                    { new: true }
-                )
+                    { $addToSet: { savedBooks: args } },
+                    { new: true, runValidators: true }
+                );
                 return updatedUser;
             }
-            throw new AuthenticationError('You need to be logged in!')
+            throw new AuthenticationError('Could not save book')
         },
-        removeBook: async (parent, { bookId }, context) => {
+        //removes a book's data from savedBooks
+        deleteBook: async (parent, { bookId }, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
-                    {_id: context.user._id},
+                    { _id: context.user._id },
                     { $pull: { savedBooks: { bookId: bookId } } },
                     { new: true }
-                )
-                return updatedUser;
+                );
+                return updatedUser
             }
+            throw new AuthenticationError('Could not delete book')
         }
     }
 }
 
-module.exports = resolvers;
+module.exports = resolvers
