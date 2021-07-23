e53ec0a9112a4fb14aee15fb00412aa37c56eb74
dustingottlieb <dustingottlieb@gmail.com>
2021-07-23 14:11:03 -0500

    fixing server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./Schemas');
 const { authMiddleware } = require('./utils/auth');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const server = new ApolloServer({
   typeDefs,
